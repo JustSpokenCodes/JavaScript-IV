799e99355ab231e9577dd1be43435e132757373b
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-04-04 19:29:21 -0700

    i hope im in the right directino

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+class People{
+    constructor(name, age, location, gender) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.gender = gender;
+    }
+    speak(){
+        return "Hello my name is Fred, I am from Bedrock where name and location."
+    }
+}
+
